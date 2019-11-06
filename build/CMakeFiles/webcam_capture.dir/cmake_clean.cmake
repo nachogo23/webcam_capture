e61cff9375db3fb64fcc53467fcefeb670832fef
nachogo23 <nachogo23@gmail.com>
2019-11-06 12:35:46 +0100

    S'afegeix el directori build i una imatge de la webcam

---
+++ build/CMakeFiles/webcam_capture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webcam_capture.dir/src/webcam_capture.cpp.o"
+  "webcam_capture.pdb"
+  "webcam_capture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webcam_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
